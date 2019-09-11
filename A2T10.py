09de768ad957dea535f673030a63369caca839e4
Rubel Biswas <45450940+Rubel15@users.noreply.github.com>
2019-09-11 23:48:44 +0600

    Add files via upload

---
+++ A2T10.py
@@ -0,0 +1,18 @@
+m=int(input("Enter number off row : "))
+s=m2=m-1
+
+
+for i in range(0,m,1):
+    for j in range(0,m,1):
+        if j<s :
+            print(" ",end="")
+        else :
+            if i==m2 or j==m-1 or j==s:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        if(j==m-1):
+            print('\n',end="")
+
+    s = s-1
+    m=m+1
